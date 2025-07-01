b06ee77a2a940302a39766bab838d5c9a63cd345
Yoshwa006 <ryoshwaa@gmail.com>
2025-07-02 00:11:13 +0530

    Added needed DTOs

---
+++ src/main/java/com/example/comp/dto/EmailAndId.java
@@ -0,0 +1,22 @@
+package com.example.comp.dto;
+
+public class EmailAndId {
+    private String email;
+    private int id;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/comp/dto/EmailAnsQues.java
@@ -0,0 +1,23 @@
+package com.example.comp.dto;
+
+public class EmailAnsQues {
+    String email;
+
+    public int getQuesId() {
+        return quesId;
+    }
+
+    public void setQuesId(int quesId) {
+        this.quesId = quesId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    int quesId;
+}
